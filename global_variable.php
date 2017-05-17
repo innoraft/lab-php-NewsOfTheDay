d3be5f7e1d38b5882bea52233d1383e8a68b9500
Rudranil Sarkar <rudranil.sarkar@innoraft.com>
2017-05-17 12:13:24 +0530

    Update global_variable.php

--- global_variable.php
+++ global_variable.php
@@ -5,7 +5,7 @@ $username = 'root';
 $password = '******';
 $database_name='tech_mail';
 
-$sending_like_mail = "localhost";	// like url in sending.php, change the path
-$delete_email="localhost"; // inactive user url in sending.php
+$sending_like_mail = "https://domain name";	// like url in sending.php, change the path
+$delete_email="https://domain name"; // inactive user url in sending.php
 
 ?>
